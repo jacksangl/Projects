12e303e3acd0d19407da4a79408deac2480227e7
jacksangl <sangljack06@gmail.com>
2025-03-06 15:59:12 -0500

    fixed warnigns

--- homework/linked-list/LabProj7.cpp
+++ homework/linked-list/LabProj7.cpp
@@ -37,7 +37,7 @@ public:
 
 int len(Node *llist)
 {
-    int length;
+    int length = 0;
     while (llist != NULL)
     {
         llist = llist->next;
@@ -59,14 +59,18 @@ void Remove(Node *&llist, unsigned startIndex, unsigned endIndex)
     }
     else if (startIndex == 0)
     {
-        while (currentIndex <= endIndex)
+        while (currentIndex <= endIndex && temp != NULL)
         {
             store = temp;
             temp = temp->next;
             currentIndex++;
         }
-        store->next = NULL;
+        if (store != NULL)
+        {
+            store->next = NULL;
+        }
         llist = temp;
+        return;
     }
     else if (endIndex == length)
     {
@@ -76,7 +80,11 @@ void Remove(Node *&llist, unsigned startIndex, unsigned endIndex)
             currentIndex++;
         }
         store = temp;
-        store->next = NULL;
+        if (store != NULL)
+        {
+            store->next = NULL;
+        }
+        return;
     }
     else
     {
@@ -91,47 +99,19 @@ void Remove(Node *&llist, unsigned startIndex, unsigned endIndex)
             temp = temp->next;
             currentIndex++;
         }
-        store->next = temp;
+        if (store != NULL)
+        {
+            store->next = temp;
+        }
     }
 
     return;
 }
 
-/*
- if (startIndex == 0) {
-        while (currentIndex < startIndex && first != NULL) {
-            first= first->next;
-            temp = temp->next;
-            currentIndex++;
-        }
-    }
-
-    int length = len(llist);
-    while (currentIndex < startIndex) {
-        temp = temp->next;
-        currentIndex++;
-    }
-    store = temp;
-
-    temp=temp->next;
-    deleter = temp;
-    while (currentIndex < endIndex) {
-        temp=temp->next;
-        deleter->next = NULL;
-        deleter = temp;
-        currentIndex++;
-    }
-
-    if (length-1 == endIndex)
-        store->next = NULL;
-    else
-        store->next = temp;
-*/
-
 // Problem B: Reversing a linked list.
 void ReverseList(Node *&llist)
 {
-    if (llist == NULL || llist == nullptr)
+    if (llist == NULL)
         return;
 
     int length = len(llist) + 1;
@@ -173,12 +153,12 @@ void ReverseList(Node *&llist)
 // Problem C: Move the smallest node to the head of a linked list.
 void MoveSmallestToHead(Node *&llist)
 {
-    if (llist == NULL || llist == nullptr)
+    if (llist == NULL)
         return;
 
     // defines my vars **temp iterates through and previous_temp does the same with the previous address however);
     // store takes the node we want to move, previous takes the before node so we can point it to the next node stored in next
-    Node *current = llist, *store = llist, *storepoint = llist, *previous = llist, *next, *storeprevious = llist;
+    Node *current = llist, *store = llist, *storepoint = llist, *previous = llist, *storeprevious = llist;
 
     // this makes sure the previous node is always 1 behind temp;
     bool once = false;
@@ -188,7 +168,7 @@ void MoveSmallestToHead(Node *&llist)
         return;
 
     current = llist->next;
-    while (current != NULL || current != nullptr)
+    while (current != NULL)
     {
         if (current->data < lowest)
         {
@@ -238,7 +218,7 @@ bool IsPalindrome(Node *llist)
         llist = llist->next;
     }
 
-    for (int i = 0, j = count-1; i < count / 2; i++, j--)
+    for (int i = 0, j = count - 1; i < count / 2; i++, j--)
     {
         if (linked[i] != linked[j])
             return false;
@@ -503,6 +483,113 @@ int main()
     cout << endl;
 #endif
 }
+
+/*
+my output
+Problem A, test case 1, removing [2]-[4]:
+2 5 8 10 15
+Result List:
+2 5
+
+Problem A, test case 2, removing [0]-[3]:
+1 6 8 13 15 26 30
+Result List:
+15 26 30
+
+problem A, test case 3, removing [0]-[0]:
+1
+result list:
+<<empty list>>
+
+problem A, test case 4, removing [0]-[6]:
+1 6 8 13 15 26 30
+result list:
+<<empty list>>
+
+Problem A, test case 5, removing [1]-[3]:
+1 6 8 13 15 26 30
+Result List:
+1 15 26 30
+
+
+///////////////////////////////////////////////////
+
+Problem B, test case 1, reversing list:
+2 5 8 10 15
+Reversed List:
+15 10 8 5 2
+
+Problem B, test case 2, reversing list:
+1 6 8 13 15 26 30
+Reversed List:
+30 26 15 13 8 6 1
+
+Problem B, test case 3, reversing list:
+1
+Reversed List:
+1
+
+Problem B, test case 4, reversing list:
+1 2
+Reversed List:
+1 2
+
+Problem B, test case 5, reversing list:
+<<empty list>>
+Reversed List:
+<<empty list>>
+
+
+///////////////////////////////////////////////////
+
+Problem C, test case 1, move smallest to head:
+8 5 10 2 15
+Resulting List:
+2 8 5 10 15
+
+Problem C, test case 2, move smallest to head:
+30 26 15 13 8 6 1
+Resulting List:
+1 30 26 15 13 8 6
+
+Problem C, test case 3, move smallest to head:
+1
+Move smallest to head:
+1
+
+Problem C, test case 4, move smallest to head:
+1 30 13 6 8 1 26
+Move smallest to head:
+1 30 13 6 8 1 26
+
+Problem C, test case 5, move smallest to head:
+<<empty list>>
+Move smallest to head:
+<<empty list>>
+
+
+///////////////////////////////////////////////////
+
+Problem D, test case 1, test if palindrome:
+1 4 6 7 6 4 1
+True
+
+Problem D, test case 2, test if palindrome:
+1 4 6 7 6 4
+False
+
+Problem D, test case 3, test if palindrome:
+1 4 6 7 4 1
+False
+
+Problem D, test case 4, test if palindrome:
+<<empty list>>
+False
+
+Problem D, test case 5, test if palindrome:
+1
+True
+*/
 
 /*
 Test Run Output
